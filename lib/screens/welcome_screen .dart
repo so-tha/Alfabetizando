2b14874c56bd4e5cc66105534844568b81545c3a
so-tha <thais3225@gmail.com>
2024-08-16 15:05:18 -0300

    deletei algumas pastas inuteis

--- lib/screens/welcome_screen .dart
+++ lib/screens/welcome_screen .dart
@@ -11,23 +11,23 @@ class _WelcomeScreen extends State<WelcomeScreen>
     with SingleTickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
-    return Scaffold( 
+    return Scaffold(
       body: Stack(
-         //permite a sobreposição
+        //permite a sobreposição
         children: [
           Container(
             decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('/home/thais/Documents/workspace/alfabetizando/lib/assets/images/background_image.jpg'),
-                fit: BoxFit.cover, //toda a pagina
-              )
-            ),
+                image: DecorationImage(
+              image: AssetImage(
+                  '/home/thais/Documents/workspace/alfabetizando/lib/assets/images/background_image.jpg'),
+              fit: BoxFit.cover, //toda a pagina
+            )),
           ),
           Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                 const Text(
+                const Text(
                   'Alfabetizando',
                   style: TextStyle(
                     fontSize: 32,
@@ -35,12 +35,20 @@ class _WelcomeScreen extends State<WelcomeScreen>
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-                const SizedBox(height: 20,),
+                const SizedBox(
+                  height: 20,
+                ),
                 ElevatedButton(
+                  style: ButtonStyle(
+                    padding:
+                        WidgetStateProperty.all<EdgeInsets>(EdgeInsets.all(20)),
+                  ),
                   onPressed: () {},
                   child: const Text('Login'),
                 ),
-                const SizedBox(height: 20,),
+                const SizedBox(
+                  height: 20,
+                ),
                 ElevatedButton(
                   onPressed: () {},
                   child: const Text('Registre-se'),
@@ -50,7 +58,7 @@ class _WelcomeScreen extends State<WelcomeScreen>
           ),
         ],
       ),
-       backgroundColor: Colors.transparent,
+      backgroundColor: Colors.transparent,
     );
   }
 }
