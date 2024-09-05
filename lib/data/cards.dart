b5f97a351cfe1221f88cf4c83763d67c588dc030
so-tha <thais3225@gmail.com>
2024-09-04 21:04:19 -0300

    consulta aos cards no banco de dados está funcionando(acho)

--- lib/data/cards.dart
+++ lib/data/cards.dart
@@ -26,23 +26,23 @@ class Category {
   }
 }
 
-// Função para buscar categorias do banco de dados
-Future<List<Category>> fetchCategories() async {
-  final response = await Supabase.instance.client.from('cards').select();
 
-  try {
-    await response.from('cards').select();
-  } on PostgrestException catch (error) {
-    error.code; // Contains PostgREST error code
+Future<List<Category>> fetchCategories() async {
+  final response = await Supabase.instance.client
+      .from('cards')
+      .select();
+
+  if (response.isEmpty) {
+    throw Exception('Nenhuma categoria encontrada.');
   }
 
-  final List<dynamic> data = response.data as List<dynamic>;
+  final List<dynamic> data = response as List<dynamic>;
   return data
       .map((json) => Category.fromJson(json as Map<String, dynamic>))
       .toList();
 }
 
-// Função para adicionar uma nova categoria
+
 Future<void> addCategory(Category category) async {
   final response =
       await Supabase.instance.client.from('cards').insert(category.toJson());
@@ -52,7 +52,8 @@ Future<void> addCategory(Category category) async {
   }
 }
 
-// Função para atualizar uma categoria existente
+
+
 Future<void> updateCategory(Category category) async {
   final response = await Supabase.instance.client
       .from('cards')
@@ -64,7 +65,7 @@ Future<void> updateCategory(Category category) async {
   }
 }
 
-// Função para remover uma categoria
+
 Future<void> deleteCategory(String id) async {
   final response =
       await Supabase.instance.client.from('cards').delete().eq('id', id);
