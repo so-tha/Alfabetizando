742ad1dc9b407062667ce84d12d0cc66e6253d84
so-tha <thais3225@gmail.com>
2024-09-24 19:52:05 -0300

    fix: divisão da parte da ui e da logica dos cards internos de cada categoria

--- lib/src/pages/cards_internos_page.dart
+++ lib/src/pages/cards_internos_page.dart
@@ -1,24 +1,12 @@
+import 'package:alfabetizando_tcc/src/ui/custom_intern_card.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:audioplayers/audioplayers.dart';
-import '../models/intern.dart'; // Importar o arquivo onde está o fetchCategories
-
-class CardsInternos {
-  final String name;
-  final String imageUrl;
-  final String soundUrl;
-
-  CardsInternos({
-    required this.name,
-    required this.imageUrl,
-    required this.soundUrl,
-  });
-}
+import '../models/intern.dart';
 
 class CardsInternosPage extends StatefulWidget {
-  final int categoryId; 
+  final int categoryId;
   final String categoryName;
-  CardsInternosPage({super.key, required this.categoryId, required this.categoryName});
+  CardsInternosPage(
+      {super.key, required this.categoryId, required this.categoryName});
 
   @override
   _CardsInternosPageState createState() => _CardsInternosPageState();
@@ -30,13 +18,15 @@ class _CardsInternosPageState extends State<CardsInternosPage> {
   @override
   void initState() {
     super.initState();
-   
-     _cardsInternosFuture = fetchCardsInternos(widget.categoryId).then((categories) {
+
+    _cardsInternosFuture =
+        fetchCardsInternos(widget.categoryId).then((categories) {
       return categories.map((category) {
         return CardsInternos(
           name: category.name,
-          imageUrl: category.imageUrl, 
+          imageUrl: category.imageUrl,
           soundUrl: category.soundUrl,
+          id: category.id,
         );
       }).toList();
     });
@@ -46,7 +36,7 @@ class _CardsInternosPageState extends State<CardsInternosPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.categoryName), 
+        title: Text(widget.categoryName),
       ),
       body: FutureBuilder<List<CardsInternos>>(
         future: _cardsInternosFuture,
@@ -75,61 +65,6 @@ class _CardsInternosPageState extends State<CardsInternosPage> {
           }
         },
       ),
-    );
-  }
-}
-
-class CardsInternosCard extends StatelessWidget {
-  final CardsInternos cardsInternos;
-  final AudioPlayer _audioPlayer = AudioPlayer();
-
-  CardsInternosCard({required this.cardsInternos});
-
-  void _playSound() async {
-    await _audioPlayer.play(AssetSource(cardsInternos.soundUrl));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        GestureDetector(
-          onTap: _playSound,
-          child: Stack(
-            alignment: Alignment.bottomCenter,
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(12.0),
-                child: Image.asset(
-                  cardsInternos.imageUrl,
-                  height: 100,
-                  width: double.infinity,
-                  fit: BoxFit.cover,
-                ),
-              ),
-              Container(
-                color: Colors.black54,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 4.0),
-                  child: Text(
-                    cardsInternos.name,
-                    style: GoogleFonts.nunito(
-                      fontSize: 16,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-        const SizedBox(height: 8),
-        IconButton(
-          icon: const Icon(Icons.volume_up),
-          onPressed: _playSound,
-        ),
-      ],
     );
   }
 }
