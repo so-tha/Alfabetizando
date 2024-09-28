e710ec3958d6011aeacd40eb70558ca18ccc4640
so-tha <thais3225@gmail.com>
2024-09-28 11:15:16 -0300

    pequena mod no delete para mostrar pop-up

--- lib/src/widgets/delete_record.dart
+++ lib/src/widgets/delete_record.dart
@@ -3,11 +3,42 @@ import 'package:flutter/material.dart';
 class DeleteRecord extends StatelessWidget {
   const DeleteRecord({super.key});
 
+  void _showDeleteConfirmation(BuildContext context, String recordingName) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Apagar Gravação'),
+          content: Text('Você tem certeza que deseja apagar "$recordingName"?'),
+          actions: [
+            TextButton.icon(
+              icon: Icon(Icons.close, color: Colors.red),
+              label: const Text('Não'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton.icon(
+              icon: Icon(Icons.check, color: Colors.green),
+              label: const Text('Sim'),
+              onPressed: () {
+              
+                Navigator.of(context).pop(); 
+                
+                
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Audio e Voz'),
+        title: const Text('Áudio e Voz'),
         backgroundColor: Colors.orange.shade200,
       ),
       body: Column(
@@ -23,7 +54,7 @@ class DeleteRecord extends StatelessWidget {
                 borderRadius: BorderRadius.circular(20.0),
                 child: Container(
                   color: Colors.pink.shade50,
-                  padding: EdgeInsets.all(16.0),
+                  padding: const EdgeInsets.all(16.0),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
@@ -42,21 +73,26 @@ class DeleteRecord extends StatelessWidget {
                           color: Colors.grey,
                         ),
                       ),
-                      //apagar diretamente no bd
-                      SizedBox(height: 16),
+                      const SizedBox(height: 16),
                       ListTile(
-                        title: Text('Voz da Mamãe'),
-                        onTap: () {},
+                        title: const Text('Voz da Mamãe'),
+                        onTap: () {
+                          _showDeleteConfirmation(context, 'Voz da Mamãe');
+                        },
                       ),
                       ListTile(
-                        title: Text('Voz padrão masculina'),
-                        onTap: () {},
+                        title: const Text('Voz padrão masculina'),
+                        onTap: () {
+                          _showDeleteConfirmation(context, 'Voz padrão masculina');
+                        },
                       ),
                       ListTile(
-                        title: Text('Voz padrão feminina'),
-                        onTap: () {},
+                        title: const Text('Voz padrão feminina'),
+                        onTap: () {
+                          _showDeleteConfirmation(context, 'Voz padrão feminina');
+                        },
                       ),
-                      Spacer(),
+                      const Spacer(),
                     ],
                   ),
                 ),
