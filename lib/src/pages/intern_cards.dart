407113fdac107e6c608317cb227c888da32c7087
so-tha <thais3225@gmail.com>
2024-10-03 13:47:35 -0300

    metade da logica do audio foi implementada

--- lib/src/pages/intern_cards.dart
+++ lib/src/pages/intern_cards.dart
@@ -1,5 +1,4 @@
 // ignore_for_file: must_be_immutable
-
 import 'package:alfabetizando_tcc/src/models/intern.dart';
 import 'package:alfabetizando_tcc/src/pages/card_detail_page.dart';
 import 'package:alfabetizando_tcc/src/ui/custom_category_card.dart';
@@ -73,7 +72,6 @@ class _CardsInternosPageState extends State<CardsInternosPage> {
           } else {
             widget.categoryName = 'Sem Nome';
           }
-
           return Scaffold(
             appBar: AppBar(
               title: Text(
@@ -93,28 +91,74 @@ class _CardsInternosPageState extends State<CardsInternosPage> {
                 mainAxisSpacing: 10,
                 childAspectRatio: 1.5,
               ),
-              itemCount: categories.length,
               itemBuilder: (context, index) {
-                final category = categories[index];
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => CardDetailPage(
+                if (index < categories.length) {
+                  final category = categories[index];
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => CardDetailPage(
+                            title: category.name,
+                            imageUrl: category.imageUrl,
+                          ),
+                        ),
+                      );
+                    },
+                    child: Stack(
+                      children: [
+                        CategoryCard(
                           title: category.name,
                           imageUrl: category.imageUrl,
+                          categoryId: category.id,
+                          categoryName: category.name,
                         ),
-                      ),
-                    );
-                  },
-                  child: CategoryCard(
-                    title: category.name,
-                    imageUrl: category.imageUrl,
-                    categoryId: category.id,
-                    categoryName: category.name,
-                  ),
-                );
+                        Positioned(
+                          top: 8,
+                          right: 8,
+                          child: IconButton(
+                            icon: Icon(Icons.volume_up),
+                            color: Colors.black,
+                            iconSize: 24.0,
+                            onPressed: () async {
+                              if (category.soundUrl.isNotEmpty) {
+                                try {
+                                  final soundUrl =
+                                      'https://bqdmmkkmjblovfvefazq.supabase.co/storage/v1/object/public/audios/${category.soundUrl}';
+                                  await _audioPlayer.setSourceUrl(soundUrl);
+                                  await _audioPlayer.resume();
+                                } catch (e) {
+                                  try {
+                                    await _audioPlayer.setSource(AssetSource(
+                                        'assets/audios/${category.soundUrl}'));
+                                    await _audioPlayer.resume();
+                                  } catch (e) {
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      SnackBar(
+                                        content: Text(
+                                            'Áudio não disponível para ${category.name}.'),
+                                      ),
+                                    );
+                                  }
+                                }
+                              } else {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                    content: Text(
+                                        'Áudio não disponível para ${category.name}.'),
+                                  ),
+                                );
+                              }
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                } else {
+                  return SizedBox();
+                }
               },
             ),
           );
